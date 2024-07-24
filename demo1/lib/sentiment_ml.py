c2ee2ab95d2965af85ce6b3c8d174ffd8ab69c1f
Erik Nielsen <erik.vishal.nielsen@gmail.com>
2024-07-24 20:44:26 +0000

    Changes

--- demo1/lib/sentiment_ml.py
+++ demo1/lib/sentiment_ml.py
@@ -16,11 +16,11 @@ def my_api(ticker, start, end):
     newsApiToken = "66a11a43014e19.64423066"
 
     # Example:
-    response = requests.get("https://api.findl.com/v1.0/data/stocks?format=json&apiKey=b88ce70b-7d65-4d39-a92c-8ce68da91965&fields=date,open,close,volume&ticker=" + ticker + "&from=" + start + "&to=" + end) 
+    priceData = requests.get("https://api.polygon.io/v2/aggs/ticker/" + ticker + "/range/1/day/" + start + "/" + end + "?adjusted=true&sort=asc&apiKey=SKIoSufYtDsfh8YitV2Ue5ozoWDgERT") 
     newsData = requests.get("https://eodhd.com/api/news?s=" + ticker + ".US&offset=0&limit=1000&api_token=" + newsApiToken + "&from=" + start + "&to=" + end + "&fmt=json") 
-    print(response.status_code)
+    print(priceData.status_code)
     print(newsData.status_code)
-    return(response.json(),newsData.json())
+    return(priceData.json(),newsData.json())
 
 if len(sys.argv) != 4:
     print("Usage: python sentiment_ml.py [ticker] [starting_date] [ending_date]")
@@ -31,7 +31,7 @@ start = sys.argv[2]
 end = sys.argv[3]
 
 try: 
-    open(ticker + "_fundamentals.json", "r")
+    open("data/" + ticker + "_fundamentals.json", "r")
 except IOError:
     jsonFileFund, jsonFileNews = my_api(ticker, start, end)
     file_pathFUND = "data/" + ticker + '_fundamentals.json'  # specify your file path here
