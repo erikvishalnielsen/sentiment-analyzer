f49542596986ccd3949efaea3de9b3ac8f2047e3
Erik Nielsen <erik.vishal.nielsen@gmail.com>
2024-07-29 18:09:50 +0000

    chang es

--- main/lib/regression.ml
+++ main/lib/regression.ml
@@ -8,12 +8,24 @@ let getSlopeSentiment (datapt1 : Datapoints.Datapoint.t) (datapt2 : Datapoints.D
   (datapt2.sentiment -. datapt1.sentiment)
 ;;
 
-let regressionCorrelation (data : Datapoints.t) : float array = 
-  let correlations = [|0.0;0.0;0.0;0.0|] in
+let coefficient (ptList : (float * float) list) : float =
+  let n = Int.to_float (List.length ptList) in
+  let sumX = List.fold ptList ~init:(0.0) ~f:(fun sum pt -> (sum +. (fst pt))) in
+  let sumXSquare = List.fold ptList ~init:(0.0) ~f:(fun sum pt -> (sum +. ((fst pt) *. (fst pt)))) in
+  let sumY = List.fold ptList ~init:(0.0) ~f:(fun sum pt -> (sum +. (snd pt))) in
+  let sumYSquare = List.fold ptList ~init:(0.0) ~f:(fun sum pt -> (sum +. ((snd pt) *. (snd pt)))) in
+  let sumXY = List.fold ptList ~init:(0.0) ~f:(fun sum pt -> (sum +. ((fst pt) *. (snd pt)))) in
+
+
+  let r = ((n *. sumXY) -. (sumX *. sumY)) /. (sqrt (((n *. sumXSquare) -. (sumX *. sumX)) *. ((n *. sumYSquare) -. (sumY *. sumY)))) in
+  r
+;;
+
+let regressionCorrelation (data : Datapoints.t) : float list = 
   let modifierList = [-1;0;1;2] in
 
   let dataLen = List.length data.data in
-  let _deltaData = List.foldi data.data ~f:(fun index deltas datapt -> 
+  let deltaData = List.foldi data.data ~f:(fun index deltas datapt -> 
     if (index > 0 && index < dataLen-3) then (
       let sentimentChange = getSlopeSentiment datapt (List.nth_exn data.data (index+1)) in
       let delts = List.map modifierList ~f:(fun modify ->
@@ -25,6 +37,9 @@ let regressionCorrelation (data : Datapoints.t) : float array =
       deltas @ [delts];
     ) else deltas
   ) ~init:([]) in
+
+  let correlationList = List.init 4 ~f:(fun num -> (List.map deltaData ~f:(fun delt -> List.nth_exn delt num))) in
+  let correlations = List.fold correlationList ~f:(fun corrs floatList -> corrs @ [coefficient floatList]) ~init:([]) in
 
   correlations
 ;;
