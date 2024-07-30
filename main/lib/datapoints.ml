0ca028e9bd7b978ed4fa18707f8e6e89dae8b0a8
Sam Meddin <samuelmeddin@gmail.com>
2024-07-30 15:46:34 +0000

    changes

--- main/lib/datapoints.ml
+++ main/lib/datapoints.ml
@@ -62,32 +62,36 @@ with Sys_error error_type -> Error (Error.of_string error_type)
 
 let json_to_datapoints ticker days =
 
-  match (json_to_tuple ticker) with | Ok (sentimentsinit, priceinit) ->
-  let sentiments = List.rev (List.slice sentimentsinit 0 days) in
-  let price = List.rev (List.slice priceinit 0 days) in
-  
-  let lastPrice = ref (List.nth_exn (snd (List.nth_exn price 0)) 1) in 
+  match (json_to_tuple ticker) with 
+  | Ok (sentimentsinit, priceinit) ->
+    if days < 5 || days > (List.length sentimentsinit) - 1 || days > (List.length priceinit) - 1 then
+      Error (Error.of_string "invalid number of days")
+    else ( 
+    let sentiments = List.rev (List.slice sentimentsinit 0 days) in
+    let price = List.rev (List.slice priceinit 0 days) in
+    
+    let lastPrice = ref (List.nth_exn (snd (List.nth_exn price 0)) 1) in 
 
-  let emptyList : Datapoint.t list = [] in
-  let dataList : Datapoint.t list = List.foldi sentiments ~init:(emptyList) ~f:(fun ind currData currSentiment -> 
-    let date = fst currSentiment in
-    let found = List.find price ~f:(fun item -> String.equal (fst item) date) in
-    match found with
-    | Some item -> (
-      let newData : Datapoint.t = {date = Finviz_parser.get_date_from_json date ; price = List.nth_exn (snd item) 1 ; 
-      sentiment = (List.fold (snd currSentiment) ~init:(0.0) ~f:(fun sum currItem -> sum +. currItem)) /. (Int.to_float (List.length (snd currSentiment))) } in
-      lastPrice := (List.nth_exn (snd item) 1);
-      (currData @ [newData]);
-    )
-    | None -> (
-      let newData : Datapoint.t = {date = Finviz_parser.get_date_from_json date ; price = !lastPrice ; 
-      sentiment = (List.fold (snd currSentiment) ~init:(0.0) ~f:(fun sum currItem -> sum +. currItem)) /. (Int.to_float (List.length (snd currSentiment))) } in
-      (currData @ [newData]);
-    )
-  ) in
-  let dataptList = {data = dataList ; price_high = (match (List.max_elt dataList ~compare:(fun item1 item2 -> if (Float.(>.) (item1.price) (item2.price)) then 1 else -1)) with | Some item -> item.price | None -> failwith "error")
-  ; price_low = match (List.min_elt dataList ~compare:(fun item1 item2 -> if (Float.(>.) (item1.price) (item2.price)) then 1 else -1)) with | Some item -> item.price | None -> failwith "error"} in
-  Ok dataptList
+    let emptyList : Datapoint.t list = [] in
+    let dataList : Datapoint.t list = List.foldi sentiments ~init:(emptyList) ~f:(fun ind currData currSentiment -> 
+      let date = fst currSentiment in
+      let found = List.find price ~f:(fun item -> String.equal (fst item) date) in
+      match found with
+      | Some item -> (
+        let newData : Datapoint.t = {date = Finviz_parser.get_date_from_json date ; price = List.nth_exn (snd item) 1 ; 
+        sentiment = (List.fold (snd currSentiment) ~init:(0.0) ~f:(fun sum currItem -> sum +. currItem)) /. (Int.to_float (List.length (snd currSentiment))) } in
+        lastPrice := (List.nth_exn (snd item) 1);
+        (currData @ [newData]);
+      )
+      | None -> (
+        let newData : Datapoint.t = {date = Finviz_parser.get_date_from_json date ; price = !lastPrice ; 
+        sentiment = (List.fold (snd currSentiment) ~init:(0.0) ~f:(fun sum currItem -> sum +. currItem)) /. (Int.to_float (List.length (snd currSentiment))) } in
+        (currData @ [newData]);
+      )
+    ) in
+    let dataptList = {data = dataList ; price_high = (match (List.max_elt dataList ~compare:(fun item1 item2 -> if (Float.(>.) (item1.price) (item2.price)) then 1 else -1)) with | Some item -> item.price | None -> failwith "error")
+    ; price_low = match (List.min_elt dataList ~compare:(fun item1 item2 -> if (Float.(>.) (item1.price) (item2.price)) then 1 else -1)) with | Some item -> item.price | None -> failwith "error"} in
+    Ok dataptList)
   | Error error_type -> Error error_type
   
 ;;
