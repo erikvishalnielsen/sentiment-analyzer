a9ced6d02d2a00a46c4dd33361454a7704ce20ca
Erik Nielsen <erik.vishal.nielsen@gmail.com>
2024-08-09 14:40:08 +0000

    changes

--- main/lib/sentiment_ml.py
+++ main/lib/sentiment_ml.py
@@ -53,10 +53,11 @@ def my_api(initTicker, start, end):
     ticker = initTicker.upper()
 
     newsApiTokenErik = "66aa4b2f971169.77002381"
+    newsApiTokenErik2 = "66b629f9b45483.89321071"
     newsApiTokenSam = "66a11a43014e19.64423066"
     # Example:
     priceData = requests.get("https://api.polygon.io/v2/aggs/ticker/" + ticker + "/range/1/day/" + start + "/" + end + "?adjusted=true&sort=desc&apiKey=SKIoSufYtDsfh8YitV2Ue5ozoWDgERT_") 
-    newsData = requests.get("https://eodhd.com/api/news?s=" + ticker + ".US&offset=0&limit=1000&api_token=" + newsApiTokenSam + "&from=" + start + "&to=" + end + "&fmt=json") 
+    newsData = requests.get("https://eodhd.com/api/news?s=" + ticker + ".US&offset=0&limit=1000&api_token=" + newsApiTokenErik2 + "&from=" + start + "&to=" + end + "&fmt=json") 
     print(priceData.status_code)
     print(newsData.status_code)
     return(priceData.json(),newsData.json())
